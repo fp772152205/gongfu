999356fa79d09b0947b19763deda48d08034f44a
fp772152205 <50104734+fp772152205@users.noreply.github.com>
2019-07-15 16:46:10 +0800

    Add files via upload

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,20 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as actions from './actions'
-import * as getters from './getters'
+import * as actions from './actions' //异步更改数据
+import * as getters from './getters' //获取数据
 
 Vue.use(Vuex)
 
 
 // 应用初始状态
 const state = {
-
-    user: JSON.parse(sessionStorage.getItem('user')) || null,
-    token: sessionStorage.getItem('token') || '',
+    user: JSON.parse(sessionStorage.getItem('user')) || null, //拿到的是json字符串成js对象
+    token: sessionStorage.getItem('token') || '', //token是对象
     router: [],
 }
 
-// 定义所需的 mutations
+// 定义所需的 mutations  同步更改全局数据
 const mutations = {
     INCREMENT(state) {
         state.count++
@@ -24,12 +23,12 @@ const mutations = {
     },
     // 用户信息
     user(state, user) {
-        var _user = sessionStorage.getItem('user');
+        var _user = sessionStorage.getItem('user'); //获取对象
         if (!_user) {
-            sessionStorage.setItem("user", JSON.stringify(user));
-            _user = JSON.parse(sessionStorage.getItem('user'));
+            sessionStorage.setItem("user", JSON.stringify(user)); //转字符串
+            _user = JSON.parse(sessionStorage.getItem('user')); //转js对象
         } else {
-            _user = JSON.parse(_user);
+            _user = JSON.parse(_user);  //转js对象
         }
         state.user = _user;
     },
@@ -41,11 +40,13 @@ const mutations = {
     // 用户权限路由
     router(state, router) {
         state.router = router;
+        sessionStorage.setItem('router', router)
     },
     // 退出登录
     logout(state) {
         state.token = '';
         state.user = null;
+        state.router = [];
         sessionStorage.clear();
 
     },
