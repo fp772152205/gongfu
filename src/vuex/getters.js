1426a22d06e11163b2d558cf78384dce99341809
fp772152205 <50104734+fp772152205@users.noreply.github.com>
2019-07-15 16:47:16 +0800

    Add files via upload

--- src/vuex/getters.js
+++ src/vuex/getters.js
@@ -3,12 +3,26 @@
 export const token = state => {
     return state.token;
 }
+export const router = state => {
+    if (state.router) {
+        return state.router;
+    } else {
+        var router = null;
+        var r = JSON.parse(sessionStorage.getItem('router'));
+        if (r) {
+            router = r;
+        }
+        state.router = router;
+        return router;
+
+    }
+
+}
 
 export const user = state => {
     if (state.user) {
         return state.user;
     } else {
-
         var user = null;
         var u = JSON.parse(sessionStorage.getItem("user"));
         if (u) {
